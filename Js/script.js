071715e928265342a587d7e42976c9dbd786962f
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2022-09-05 23:32:13 +0100

    new code

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = { threshold: 1 };
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+console.log(faders);
